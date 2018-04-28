f7e3727e32c4689279db5c5b7fffaa37f328c280
Serena Hathi <hathi.serena@gmail.com>
2018-04-28 13:22:24 +0100

    :white_check_mark: Top row of 2x2 box can be drawn

---
+++ lib/box_creator.rb
@@ -0,0 +1,7 @@
+class BoxCreator
+
+  def draw_top_row
+  puts "\u250C" "\u2510"
+  end
+
+end
---
+++ spec/box_creator_spec.rb
@@ -0,0 +1,12 @@
+require 'box_creator'
+
+describe BoxCreator do
+  let(:box) { described_class.new }
+  describe '#draw_top_row' do
+    it 'draws the top row of a 2x2 box' do
+      top_row = "┌┐\n"
+      expect { box.draw_top_row() }.to output(top_row).to_stdout
+    end
+  end
+end
+
