ec700282b7ed6d7100d969822711a19ce9c012db
Serena Hathi <hathi.serena@gmail.com>
2018-04-28 13:36:14 +0100

    :recycle: Refactor bottom left and bottom right corners into constants

--- lib/box_creator.rb
+++ lib/box_creator.rb
@@ -2,13 +2,15 @@ class BoxCreator
 
   TOP_LEFT_CORNER = "\u250C"
   TOP_RIGHT_CORNER = "\u2510"
+  BOTTOM_LEFT_CORNER = "\u2514"
+  BOTTOM_RIGHT_CORNER = "\u2518"
 
   def draw_top_row
     puts TOP_LEFT_CORNER + TOP_RIGHT_CORNER
   end
 
   def draw_bottom_row
-    puts "\u2514" "\u2518"
+    puts BOTTOM_LEFT_CORNER + BOTTOM_RIGHT_CORNER
   end
 
 end
