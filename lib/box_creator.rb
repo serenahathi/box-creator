568f98e1826ca6b1a38d739cacb27b9a3f4d17c3
Serena Hathi <hathi.serena@gmail.com>
2018-04-28 14:02:42 +0100

    :sparkles: A complete box of any given width can be drawn

--- lib/box_creator.rb
+++ lib/box_creator.rb
@@ -6,6 +6,11 @@ class BoxCreator
   BOTTOM_RIGHT_CORNER = "\u2518"
   HORIZONTAL_EDGE = "\u2576 "
 
+  def draw_box(width)
+    draw_top_row(width)
+    draw_bottom_row(width)
+  end
+
   def draw_top_row(width)
     puts TOP_LEFT_CORNER + HORIZONTAL_EDGE * (width - 2) + TOP_RIGHT_CORNER
   end
--- spec/box_creator_spec.rb
+++ spec/box_creator_spec.rb
@@ -2,7 +2,7 @@ require 'box_creator'
 
 describe BoxCreator do
   let(:box) { described_class.new }
-   
+
   describe "#draw_top_row" do
     it "draws the top row of a box based of any given width" do
       top_row = "┌╶ ╶ ┐\n"
@@ -14,6 +14,13 @@ describe BoxCreator do
     it "draws the bottom row of a box based of any given width" do
       bottom_row = "└╶ ╶ ┘\n"
       expect { box.draw_bottom_row(4) }.to output(bottom_row).to_stdout
+    end
+  end
+
+  describe "#draw_box" do
+    it "draws an entire box based on any given width" do
+      four_by_fourbox = "┌╶ ╶ ┐\n└╶ ╶ ┘\n"
+      expect { box.draw_box(4) }.to output(four_by_fourbox).to_stdout
     end
   end
 end
